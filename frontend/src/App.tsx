38a80ceddd153b5f1c664093e7f6a7ecebc4884b
ibhf13 <ibhf13@gmail.com>
2026-04-27 20:30:24 +0200

    Add Vercel Web Analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import AppRouter from '@/components/app/AppRouter'
 import AppProviders from '@/providers/AppProviders'
+import { Analytics } from '@vercel/analytics/react'
 import React from 'react'
 
 const App: React.FC = () => {
     return (
         <AppProviders>
             <AppRouter />
+            <Analytics />
         </AppProviders>
     )
 }
